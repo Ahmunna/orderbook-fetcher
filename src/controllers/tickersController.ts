d768b2002eaa47130095847c79fe4c25d9e417d8
Adnane Hmunna <adn.hmunna@gmail.com>
2024-04-29 16:42:34 +0200

    update setup redis docker

--- src/controllers/tickersController.ts
+++ src/controllers/tickersController.ts
@@ -1,13 +1,10 @@
 import { MarketPriceService } from '../services/marketPriceService';
 import { Request, Response } from 'express';
-const redis = require('redis');
+import Redis from 'ioredis';
 
-const redisClient = redis.createClient({
-  host: process.env.REDIS_HOST,
-  port: process.env.REDIS_PORT
-  });
+const redis = new Redis();
 
-redisClient.on('connect', () => {
+redis.on('connect', () => {
   console.log('Redis client is connected');
 });
 
@@ -16,25 +13,23 @@ redisClient.on('connect', () => {
 
     const key = `${req.query.baseAsset}${req.query.quoteAsset}`
 
-    redisClient.get(key, async (err: Error, data: string) => {
-      try
-      {
-        if(data) {
-          res.json({ result: parseFloat(data)})
-        }
-        else {
-          const marketPriceSerice = new MarketPriceService();
-          const price = await marketPriceSerice.getPrice(req.params.baseAsset, req.params.quoteAsset);
-          redisClient.setex(key, 30, price)
-          res.json({ result: price })
-        }
+    const data: string  = await redis.get(key) || '';
+    try
+    {
+      if(data) {
+        res.json({ result: parseFloat(data)})
       }
-      catch(error: any)
-      {
-        res.sendStatus(500);
+      else {
+        const marketPriceSerice = new MarketPriceService();
+        const price = await marketPriceSerice.getPrice(req.params.baseAsset, req.params.quoteAsset);
+        redis.setex(key, 30, price)
+        res.json({ result: price })
       }
-       
-    })
+    }
+    catch(error: any)
+    {
+      res.sendStatus(500);
+    }
   })
 
 
